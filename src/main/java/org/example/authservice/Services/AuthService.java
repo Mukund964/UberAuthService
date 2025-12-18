536aba332455b5aa176da454c7ef456651fa4af4
Mukund Kumar <mukund@Mukunds-MacBook-Air.local>
2025-12-19 00:44:28 +0530

    Added bcrypt encryption algo

--- src/main/java/org/example/authservice/Services/AuthService.java
+++ src/main/java/org/example/authservice/Services/AuthService.java
@@ -6,21 +6,25 @@ import org.example.authservice.dtos.passengerDto;
 import org.example.authservice.dtos.passengerSignUpRequestDto;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthService {
     @Autowired
     private PassengerRepository passengerRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     public passengerDto signupPassenger(passengerSignUpRequestDto passengerSignUpDto){
         passenger Passenger = passenger.builder()
                 .email(passengerSignUpDto.getEmail())
                 .Name(passengerSignUpDto.getName())
-                .password(passengerSignUpDto.getPassword())
+                .password(passwordEncoder.encode(passengerSignUpDto.getPassword()))
                 .phoneNumber(passengerSignUpDto.getPhoneNumber())
                 .build();
 
+        passengerRepository.save(Passenger);
         return passengerDto.from(Passenger);
     }
 }
