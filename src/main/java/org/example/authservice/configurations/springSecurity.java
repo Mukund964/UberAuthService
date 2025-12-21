87ba61d6b064a8947ae3f2432248ddf135aba913
Mukund Kumar <mukund@Mukunds-MacBook-Air.local>
2025-12-21 16:15:16 +0530

    Added authentication provider and manager

--- src/main/java/org/example/authservice/configurations/springSecurity.java
+++ src/main/java/org/example/authservice/configurations/springSecurity.java
@@ -1,31 +1,69 @@
 package org.example.authservice.configurations;
 
+import org.example.authservice.Services.passengerAuthImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsPasswordService;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
-public class springSecurity {
+public class springSecurity implements WebMvcConfigurer {
 
     @Bean
     public DefaultSecurityFilterChain FilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(Request -> Request
-                        .requestMatchers("/api/v1/auth/signup/*").permitAll()
-//                        .requestMatchers("/api/v1/auth/signin/*").permitAll()
+                                .requestMatchers("/api/v1/auth/signup/*").permitAll()
+                        .requestMatchers("/api/v1/auth/signin/*").permitAll()
                 )
-                        .build();
+                .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailService() {
+        return new passengerAuthImpl();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider AuthenticationProvider = new DaoAuthenticationProvider(userDetailService());
+        AuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        return AuthenticationProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+
+
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
     public PasswordEncoder encoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOriginPatterns("*").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
     }
 }
