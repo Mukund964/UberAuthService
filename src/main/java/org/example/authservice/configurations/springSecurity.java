3542045e99405ea2f50dc77ff442d3b9813f5f72
Mukund Kumar <mukund@Mukunds-MacBook-Air.local>
2025-12-21 12:50:18 +0530

    started spring security

--- src/main/java/org/example/authservice/configurations/springSecurity.java
+++ src/main/java/org/example/authservice/configurations/springSecurity.java
@@ -2,11 +2,27 @@ package org.example.authservice.configurations;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.DefaultSecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class springSecurity {
+
+    @Bean
+    public DefaultSecurityFilterChain FilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(Request -> Request
+                        .requestMatchers("/api/v1/auth/signup/*").permitAll()
+//                        .requestMatchers("/api/v1/auth/signin/*").permitAll()
+                )
+                        .build();
+    }
 
     @Bean
     public PasswordEncoder encoder() {
